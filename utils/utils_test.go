d11e616e1e316374d587e2026edda2a3f4be9f40
leiyonglin <leiyonglin@gmail.com>
2015-01-23 04:24:35 +0000

    update

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,9 @@
 package utils
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 func TestDelEmptySlice(t *testing.T) {
 	s := []string{"", "a", "c", "b"}
@@ -15,4 +18,29 @@ func TestDelEmptySlice(t *testing.T) {
 			t.Error("delete empty error")
 		}
 	}
+}
+
+func TestStringReverse(t *testing.T) {
+	s := []string{"c", "a", "b"}
+	ss := []string{"c", "b", "a"}
+
+	sort.Sort(StringReverse(s))
+
+	if !compareStringSlice(s, ss) {
+		t.Errorf("reverse error %v %v", s, ss)
+	}
+}
+
+func compareStringSlice(s1, s2 []string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	for k, v := range s1 {
+		if v != s2[k] {
+			return false
+		}
+	}
+
+	return true
 }
