7adc5db33a133d61e5c175fb00d1021f1a5758bb
leiyonglin <leiyonglin@gmail.com>
2015-01-26 03:50:46 +0000

    update

--- models/cluster.go
+++ models/cluster.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"time"
-
-	"github.com/l2x/wolffy/controllers"
-)
+import "time"
 
 type Cluster struct {
 	Id      int
@@ -15,68 +11,44 @@ type Cluster struct {
 	Created time.Time
 }
 
+func (m Cluster) TableName() string {
+	return "cluster"
+}
+
 func (m Cluster) Search(name string) ([]*Cluster, error) {
-	sqlStr := `SELECT id, name, room, machine, note, created 
-			   FROM cluster 
-			   WHERE name = ?`
-	stmt, err := DB.Prepare(sqlStr)
+	var clusters []*Cluster
+
+	_, err := DB.QueryTable(m.TableName()).Filter("name__contains", name).All(&clusters)
 	if err != nil {
 		return nil, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(name)
-	if err != nil {
-		return nil, err
-	}
-
-	clusters := []*Cluster{}
-	for rows.Next() {
-		c := &Cluster{}
-		err = rows.Scan(&c.Id, &c.Name, &c.Room, &c.Machine, &c.Note, &c.Created)
-		if err != nil {
-			continue
-		}
-		clusters = append(clusters, c)
 	}
 
 	return clusters, nil
 }
 
 func (m Cluster) Get(id int) (*Cluster, error) {
-	sqlStr := `SELECT id, name, room, machine, note, created 
-			   FROM cluster 
-			   WHERE id = ?
-			   LIMIT 1`
-	stmt, err := DB.Prepare(sqlStr)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
+	var cluster *Cluster
 
-	c := &Cluster{}
-	err = stmt.QueryRow(id).Scan(&c.Id, &c.Name, &c.Room, &c.Machine, &c.Note, &c.Created)
+	_, err := DB.QueryTable(m.TableName()).Filter("id", id).All(&cluster)
 	if err != nil {
 		return nil, err
 	}
 
-	return c, nil
+	return cluster, nil
 }
 
 func (m Cluster) Add(name, room, machine, note string) (int, error) {
-	sqlStr := `INSERT INTO cluster(name, room, machine, note, created)
-			   VALUES(?, ?, ?, ?, ?)`
-	stmt, err := DB.Prepare(sqlStr)
-	if err != nil {
-		return 0, err
+	cluster := &Cluster{
+		Name:    name,
+		Room:    room,
+		Machine: machine,
+		Note:    note,
+		Created: time.Now(),
 	}
-	defer stmt.Close()
-	created := time.Now().Format(controllers.DatetimeFormat)
-	res, err := stmt.Exec(name, room, machine, note, created)
+	id, err := DB.Insert(&cluster)
 	if err != nil {
 		return 0, err
 	}
 
-	id, _ := res.LastInsertId()
 	return int(id), nil
 }
