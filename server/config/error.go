6a63b443fedba777bd86e710f77e784d22450fa1
leiyonglin <leiyonglin@gmail.com>
2015-03-19 03:36:19 +0000

    update

---
+++ server/config/error.go
@@ -0,0 +1,28 @@
+package config
+
+import "errors"
+
+var (
+	ERR_OK      = 0
+	ERR_UNKNOWN = 1000
+
+	ERR_SESSION_INVALID = 1001
+
+	ERR_USER_NEED_CHANGE_PWD = 2001
+	ERR_USER_NOT_ADMIN       = 2002
+
+	ERR = map[int]string{
+		ERR_OK:              "ok",
+		ERR_UNKNOWN:         "unknown error",
+		ERR_SESSION_INVALID: "session invalid",
+	}
+)
+
+func GetErr(errno int) error {
+	e, ok := ERR[errno]
+	if !ok {
+		e = ERR[ERR_UNKNOWN]
+	}
+
+	return errors.New(e)
+}
