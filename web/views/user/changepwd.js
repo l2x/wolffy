2a882ffff56e222e07bd1faf0624a44aebd3e8e0
leiyonglin <leiyonglin@gmail.com>
2015-04-06 15:31:08 +0000

    update

---
+++ web/views/user/changepwd.js
@@ -0,0 +1,27 @@
+"use strict";
+
+define(['app', '../service/user'], function (app) {
+    return ['$scope', '$rootScope', '$location', 'User.Changepwd',
+	     function ($scope, $rootScope, $location, Changepwd) {
+			$scope.args = {}
+			$scope.ev = {}
+			$scope.args.user = {}
+
+			$scope.ev.save = function() {
+				if(!$scope.changepwdform.$valid) {
+					return false
+				}
+
+				Changepwd.query($scope.args.user, function(json) {
+					if($rootScope.checkErr(json)) {
+						return
+					}
+
+					$location.path("/login")
+				})
+
+			}
+
+
+        }];
+});
